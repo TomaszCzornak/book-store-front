bed3b7ffaca202a97d5e6390a5613a30910d8d59
TomaszCzornak <104570701+TomaszCzornak@users.noreply.github.com>
2023-08-22 16:08:22 +0200

    valueChanges comparing dates and lastnames

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -8,10 +8,11 @@ import {MatInputModule} from "@angular/material/input";
 import {MaterialModule} from "../core/models/material/material.module";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
 
 @NgModule({
   declarations: [AdminComponent],
-  imports: [CommonModule, AdminRoutingModule, ReactiveFormsModule, MatInputModule, MaterialModule, MatFormFieldModule, FormsModule, MatDatepickerModule],
+  imports: [CommonModule, AdminRoutingModule, ReactiveFormsModule, MatInputModule, MaterialModule, MatFormFieldModule, FormsModule, MatDatepickerModule,MatNativeDateModule],
   exports:[AdminComponent, MaterialModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule]
 })
 export class AdminModule {}
